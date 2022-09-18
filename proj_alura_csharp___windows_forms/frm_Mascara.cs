d620d0e0c7fb1f88c4d603f0f66e37583f722063
ecordeiro <edufacfito@gmail.com>
2022-09-18 18:52:30 -0300

    Manipulando as mascaras

--- proj_alura_csharp___windows_forms/frm_Mascara.cs
+++ proj_alura_csharp___windows_forms/frm_Mascara.cs
@@ -16,5 +16,72 @@ namespace proj_alura_csharp___windows_forms
         {
             InitializeComponent();
         }
+
+
+
+        private void Btn_VerConteudo_Click(object sender, EventArgs e)
+        {
+            Lbl_Conteudo.Text = Msk_TextBox.Text;
+        }
+
+        private void Btn_Hora_Click(object sender, EventArgs e)
+        {
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Msk_TextBox.Mask = "00:00";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_CEP_Click(object sender, EventArgs e)
+        {
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Msk_TextBox.Mask = "00000-000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Moeda_Click(object sender, EventArgs e)
+        {
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Msk_TextBox.Mask = "$ 000,000,000.00";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Data_Click(object sender, EventArgs e)
+        {
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Msk_TextBox.Mask = "00/00/0000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Telefone_Click(object sender, EventArgs e)
+        {
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Msk_TextBox.Mask = "(00) 0 0000-0000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Senha_Click(object sender, EventArgs e)
+        {
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.UseSystemPasswordChar = true;
+            Msk_TextBox.Mask = "000000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
     }
 }
